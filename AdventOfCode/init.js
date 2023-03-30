5ea9381e255316db8f3e7874f7d435209d36a11a
Hermes <hermeschigoziri@gmail.com>
2023-03-30 09:34:47 +0100

    better folder/file structure

--- AdventOfCode/init.js
+++
@@ -1,9 +0,0 @@
-import { join, dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-import { readFileSync } from "node:fs";
-
-export default function readInput(filePath, inputFileName = "input.txt") {
-	const __filename = fileURLToPath(filePath);
-	const __dirname = dirname(__filename);
-	return readFileSync(join(__dirname, inputFileName), "utf8").toString();
-}
